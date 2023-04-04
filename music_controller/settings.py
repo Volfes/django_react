f99397fd5adaf08b0fcc95a20e2bee96f3fb4e82
Maciej W <volfes2000@wp.pl>
2023-04-04 16:24:12 +0200

    up

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'rest_framework',
+    # 'api.apps.ApiConfig',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
